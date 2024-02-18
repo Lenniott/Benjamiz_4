1eab2c2abab89fbf69369f331c2fedbbb78ed910
Ben Kanagroo <78645337+Lenniott@users.noreply.github.com>
2024-02-18 23:09:53 +0000

    updated image links

--- src/components/molicules/BookScroll.tsx
+++ src/components/molicules/BookScroll.tsx
@@ -20,7 +20,7 @@ const Showcase: React.FC<ShowcaseProps> = ({ items, imageSize = { width: 'w-32',
   return (
     <div className="flex overflow-x-scroll py-4 scrollbar-custom">
       {items.map((item, index) => (
-        <a key={index} target="_blank" rel="noopener noreferrer" className="m-4 flex-none">
+        <a key={index} href={item.link} target="_blank" rel="noopener noreferrer" className="m-4 flex-none">
           <div className={`${imageSize.width} ${imageSize.height} overflow-hidden rounded-lg shadow-lg`}>
             <img src={item.img} alt={item.title} className="w-full h-full object-cover" />
           </div>
