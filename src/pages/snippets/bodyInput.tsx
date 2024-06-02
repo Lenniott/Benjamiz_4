e04aee699795adc361d898057a40c2e1698589c8
Ben Kanagroo <78645337+Lenniott@users.noreply.github.com>
2024-06-02 07:58:02 +0100

    front and back and colour

--- src/pages/snippets/bodyInput.tsx
+++ src/pages/snippets/bodyInput.tsx
@@ -10,6 +10,7 @@ const BodyInput: React.FC = () => {
   const [clickPosition, setClickPosition] = useState<ClickPosition | null>(null);
   const [isZoom, setIsZoom] = useState(false);
   const containerRef = useRef<HTMLDivElement | null>(null);
+  const [isBack, setIsBack] = useState(false);
 
   const handlePathClick = (event: React.MouseEvent<SVGPathElement>) => {
     const svg = event.currentTarget.ownerSVGElement;
@@ -80,14 +81,18 @@ const BodyInput: React.FC = () => {
 
   return (
     <div className="relative h-full w-full">
+      <div className="flex justify-between w-64">
       <button onClick={handleZoomToggle}>
         Toggle Zoom
       </button>
+      <button onClick={() => setIsBack(!isBack)}>{isBack ? "Front" : "Back"}</button>
+      </div>
+
       <div ref={containerRef} className="body-container border-2 h-96 w-64 overflow-auto">
         <div className={`flex items-center justify-center ${isZoom ? 'h-[1200px] w-[600px]' : 'h-[380px] w-[252px]'}`}>
           <HumanBody
             fill="currentColor"
-            className="text-accent bg-yellow-300"
+            isBack={isBack}
             onPathClick={handlePathClick}
             clickPositions={clickPosition ? [clickPosition] : []}
             isZoom={isZoom}
